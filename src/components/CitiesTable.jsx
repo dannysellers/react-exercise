e91d6ed2b538c937162e019bf3d2f0c39f130d82
Daniel Sellers <dsellers90@gmail.com>
2021-07-06 17:36:09 -0400

    update CitiesTable display info

--- src/components/CitiesTable.jsx
+++ src/components/CitiesTable.jsx
@@ -6,10 +6,16 @@ const CitiesTable = () => {
   const cities = useSelector(selectCitiesByName);
 
   return <table>
+    <thead>
+      <td>Name</td>
+      <td>Country</td>
+      <td>Likes</td>
+    </thead>
     <tbody>
     {cities.map((city, idx) => <tr key={idx}>
       <td>{city.name}</td>
       <td>{city.country}</td>
+      <td>{city.details.likes}</td>
     </tr>)}
     </tbody>
   </table>
